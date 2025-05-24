5ffff38c45ddd2912003e47cbe8f97736f4369bb
phildens <vborodulin67@gmail.com>
2025-05-24 19:06:58 +0500

    add first api route

--- FasadSiteDjango/urls.py
+++ FasadSiteDjango/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from shop_part.views import ProductListView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', ProductListView.as_view(), name='products'),
 ]
--- shop_part/views.py
+++ shop_part/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from shop_part.models import Product
+from shop_part.serializers import ProductSerializer
+from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView
 # Create your views here.
+class ProductListView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
