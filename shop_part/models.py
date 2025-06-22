9595a462541e966c58bf60e270cd36280c868e69
sashawa123 <kichiginsashawa@mail.ru>
2025-06-22 16:37:13 +0500

    add image in cat

--- shop_part/models.py
+++ shop_part/models.py
@@ -11,7 +11,7 @@ class Type(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=100)
     link_name = models.CharField(max_length=100, null=True)
-
+    card_image = models.ImageField(null=True, blank=True)
     def __str__(self):
         return self.name
 
